a19eac722405663e44f4c5e9ea495f7c396f7aa7
itwysgsl <iamstenman@protonmail.com>
2019-10-09 21:38:49 +0900

    misc: Minor fixes before the launch

--- src/util.cpp
+++ src/util.cpp
@@ -1239,7 +1239,7 @@ int GetNumCores()
 
 std::string CopyrightHolders(const std::string& strPrefix)
 {
-    std::string strCopyrightHolders = strPrefix + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
+    std::string strCopyrightHolders = "Copyright (C) 2018-2019 " + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
 
     // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
