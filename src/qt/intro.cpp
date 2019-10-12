df7a1e77c0039b91f0e84a73f0d5fa83c206e85f
itwysgsl <iamstenman@protonmail.com>
2019-10-12 10:46:46 +0900

    qt: Updated icons

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -204,7 +204,7 @@ bool Intro::pickDataDirectory(interfaces::Node& node)
         /* If current default data directory does not exist, let the user choose one */
         Intro intro;
         intro.setDataDirectory(dataDir);
-        intro.setWindowIcon(QIcon(":icons/bitcoin"));
+        intro.setWindowIcon(QIcon(":icons/icon"));
 
         while(true)
         {
